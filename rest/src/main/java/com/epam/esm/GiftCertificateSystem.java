ebd2ddfa2a27e1c1943fb75ae0cc23adc2e2cc4e
vlad-gal <vlgalathevich@gmail.com>
2021-03-08 00:43:17 +0300

    changed main class

--- rest/src/main/java/com/epam/esm/GiftCertificateSystem.java
+++ rest/src/main/java/com/epam/esm/GiftCertificateSystem.java
@@ -2,11 +2,12 @@ package com.epam.esm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.util.Locale;
 
 @SpringBootApplication
-public class GiftCertificateSystem {
+public class GiftCertificateSystem extends SpringBootServletInitializer {
     public static void main(String[] args) {
         Locale.setDefault(Locale.ENGLISH);
         SpringApplication.run(GiftCertificateSystem.class, args);
